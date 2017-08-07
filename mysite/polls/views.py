201e8dfe24fd729b4eb00dd96dd9eeec1904ca3c
hemanth7799 <hemanth7799@hotmail.com>
2017-08-07 23:41:37 +0530

    Add files via upload
    updated upto tut 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,16 +2,13 @@ from django.http import HttpResponse,Http404
 from django.template import loader
 from django.shortcuts import render
 from .models import Question
-
+from django.shortcuts import get_object_or_404
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/details.html', {'question': question})
 def results(request, question_id):
     response = "You're looking at the results of question %s."
